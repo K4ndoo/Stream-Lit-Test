41f9163218095d6489efc53c6a21f54a358dc2b4
https://gitlab.cefim-formation.org/GeorgesQuentin <georges.quentin@hotmail.com>
2026-01-13 13:14:14 +0100

    Save du dataframe

--- streamlit.py
+++ streamlit.py
@@ -1,39 +1,12 @@
 import streamlit as st
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Embedding, Dense, GlobalAveragePooling1D
+import pandas as pd
 
-import re
-import unicodedata
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
-nltk.download()
-stop_words = stopwords.words('english')
-
-# Converts the unicode file to ascii
-def unicode_to_ascii(s):
-    return ''.join(c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn')
-
-def preprocess_sentence(w):
-    w = unicode_to_ascii(w.lower().strip())
-    # creating a space between a word and the punctuation following it
-    # eg: "he is a boy." => "he is a boy ."
-    w = re.sub(r"([?.!,¿])", r" \1 ", w)
-    w = re.sub(r'[" "]+', " ", w)
-    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
-    w = re.sub(r"[^a-zA-Z?.!]+", " ", w)
-    w = re.sub(r'\b\w{0,2}\b', '', w)
-
-    # remove stopword
-    mots = word_tokenize(w.strip())
-    mots = [mot for mot in mots if mot not in stop_words]
-    return ' '.join(mots).strip()
-
-df.review = df.review.apply(lambda x :preprocess_sentence(x))
+df = pd.read_pickle('reviews_cleaned.pkl')
 
 import tensorflow as tf
+
 tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=10000)
 tokenizer.fit_on_texts(df.review)
 
